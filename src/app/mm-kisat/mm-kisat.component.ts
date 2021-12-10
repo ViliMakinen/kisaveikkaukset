39ffd7011e884b5c524d2d45e2c1cb71ab2e700f
VMakinen <viltsu.m@hotmail.com>
2021-12-10 17:12:52 +0200

    Add some example divs.

--- src/app/mm-kisat/mm-kisat.component.ts
+++ src/app/mm-kisat/mm-kisat.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mm-kisat',
   templateUrl: './mm-kisat.component.html',
   styleUrls: ['./mm-kisat.component.scss']
 })
-export class MmKisatComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class MmKisatComponent {
 
 }
