d47aba33f5a29d78ccc4563053d41e3ad4161308
Aapo Kallio <aapo.kallio@gmail.com>
2021-12-28 21:30:53 +0200

    add bracket

--- src/app/bracket/bracket.component.ts
+++ src/app/bracket/bracket.component.ts
@@ -1,4 +1,22 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, Input } from '@angular/core';
+
+export interface Bracket {
+  semiFinalists: {
+    QF1winner: null | string,
+    QF2winner: null | string,
+    QF3winner: null | string,
+    QF4winner: null | string
+  },
+  finalists: {
+    SF1winner: null | string,
+    SF2winner: null | string
+  },
+  winner: {
+    finalWinner: null | string
+  }
+}
+
 
 @Component({
   selector: 'app-bracket',
@@ -9,29 +27,71 @@ export class BracketComponent {
   @Input()
   pairs!: any;
 
-  winners = {
-    matchOneWinner: '',
-    matchTwoWinner: '',
-    matchThreeWinner: '',
-    matchFourWinner: '',
-  }
 
-  setMatchWinner(pair: any, team: string): void {
-    if (pair.seed === 1) {
-      this.winners.matchOneWinner = team;
-    } else if (pair.seed === 2) {
-      this.winners.matchTwoWinner = team;
-    } else if (pair.seed === 3) {
-      this.winners.matchThreeWinner = team;
-    } else if (pair.seed === 4) {
-      this.winners.matchFourWinner = team;
+  bracketInfo:  Bracket = {
+    semiFinalists: {
+      QF1winner: null,
+      QF2winner: null,
+      QF3winner: null,
+      QF4winner: null
+    },
+    finalists: {
+      SF1winner: null,
+      SF2winner: null
+    },
+    winner: {
+      finalWinner: null
     }
   }
 
+
+  areQuarterFinalistsNotSelected(): boolean {
+    const keyValues = Object.entries(this.bracketInfo.semiFinalists)
+    const values = keyValues.map(keyValue => keyValue[1])
+    return values.includes(null)
+  }
+
+  areSemiFinalistsNotSelected(): boolean {
+    const keyValues = Object.entries(this.bracketInfo.finalists)
+    const values = keyValues.map(keyValue => keyValue[1])
+    return values.includes(null)
+  }
+
+  setQuarterFinalWinners(pair: any, team: string): void {
+    if (pair.seed === 1) {
+      this.bracketInfo.semiFinalists.QF1winner = team;
+    } else if (pair.seed === 2) {
+      this.bracketInfo.semiFinalists.QF2winner = team;
+    } else if (pair.seed === 3) {
+      this.bracketInfo.semiFinalists.QF3winner = team;
+    } else if (pair.seed === 4) {
+      this.bracketInfo.semiFinalists.QF4winner = team;
+    }
+  }
+
+  isQuarterFinalist(team: string | null): boolean {
+    if (team === null) {
+      return false
+    }
+    if (team === this.bracketInfo.semiFinalists.QF1winner || team === this.bracketInfo.semiFinalists.QF2winner || team === this.bracketInfo.semiFinalists.QF3winner || team === this.bracketInfo.semiFinalists.QF4winner) {
+      return true
+    }
+    return false
+  }
+
+  isSemiFinalist(team: string | null): boolean {
+    if (team === null) {
+      return false
+    }
+    if (team === this.bracketInfo.finalists.SF1winner || team === this.bracketInfo.finalists.SF2winner) {
+      return true
+    }
+    return false
+  }
+
   clearAll(): void {
-    this.winners.matchOneWinner = '';
-    this.winners.matchTwoWinner = '';
-    this.winners.matchThreeWinner = '';
-    this.winners.matchFourWinner = '';
+    const SFkeyValues = Object.entries(this.bracketInfo.semiFinalists)
+    const QFkeyValues = Object.entries(this.bracketInfo.finalists)
+    const FkeyValues = Object.entries(this.bracketInfo.winner)
   }
 }
