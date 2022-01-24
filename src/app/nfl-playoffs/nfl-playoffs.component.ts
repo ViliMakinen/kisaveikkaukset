c4aa2ea796777ad3f1df13032f453cecc9fc59ea
Aapo Kallio <aapo.kallio@gmail.com>
2022-01-24 20:13:21 +0200

    asdasd

--- src/app/nfl-playoffs/nfl-playoffs.component.ts
+++ src/app/nfl-playoffs/nfl-playoffs.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-nfl-playoffs',
   templateUrl: './nfl-playoffs.component.html',
-  styleUrls: ['./nfl-playoffs.component.scss']
+  styleUrls: ['./nfl-playoffs.component.scss'],
 })
 export class NFLPlayoffsComponent {
   competition = seededTournaments[0];
@@ -15,101 +15,119 @@ export class NFLPlayoffsComponent {
 
   constructor(private userService: UserService) {
     this.user = userService.getCurrentUser();
-    userService.getUsers().then(users => {
+    userService.getUsers().then((users) => {
       this.users = users;
-      this.userIndex = this.users.findIndex(player => player.name === this.user!.name);
+      this.userIndex = this.users.findIndex((player) => player.name === this.user!.name);
     });
   }
 
   get arePredictionsLocked(): boolean {
-    return this.user!.predictions.find(prediction => prediction.tournament === 'NFL-playoffs')!.locked;
+    return this.user!.predictions.find((prediction) => prediction.tournament === 'NFL-playoffs')!
+      .locked;
   }
 
   submitUserSelections(nflBracket: NFLBracket) {
-    const newPredictions = this.user!.predictions.map(prediction => {
+    const newPredictions = this.user!.predictions.map((prediction) => {
       if (prediction.tournament === this.competition.name) {
-        return this.user!.name === 'results' ? {
-          tournament: this.competition.name,
-          predictions: nflBracket,
-          locked: false
-        } : {
-          tournament: this.competition.name,
-          predictions: nflBracket,
-          locked: true,
-        }
+        return this.user!.name === 'results'
+          ? {
+            tournament: this.competition.name,
+            predictions: nflBracket,
+            locked: false,
+          }
+          : {
+            tournament: this.competition.name,
+            predictions: nflBracket,
+            locked: true,
+          };
       } else {
-        return prediction
+        return prediction;
       }
-    })
-    console.log(newPredictions)
+    });
+    console.log(newPredictions);
     this.userService.updateUserPredictions({
       ...this.user!,
-      predictions: newPredictions
-    }).then(() => {
-      console.log('Updated successfully!');
-      this.userService.getUser(this.user!.name).then(user => this.user = user);
-    });
+      predictions: newPredictions,
+    })
+      .then(() => {
+        console.log('Updated successfully!');
+        this.userService.getUser(this.user!.name).then((user) => (this.user = user));
+      });
   }
 
-
   submitUserSelectionsBackUp(nflBracket: NFLBracket) {
-    const newPredictions = this.user!.predictions.map(prediction => {
+    const newPredictions = this.user!.predictions.map((prediction) => {
       if (prediction.tournament === this.competition.name) {
-        return this.user!.name === 'results' ? {
-          tournament: this.competition.name,
-          predictions: this.users?.find(user => user.name === 'results')?.predictions.find(prediction => prediction.tournament === 'NFL-playoffs')?.predictions as NFLBracket,
-          locked: false,
-          tournamentPoints: 0
-        } : {
-          tournament: this.competition.name,
-          predictions: this.users?.find(user => user.name === 'results')?.predictions.find(prediction => prediction.tournament === 'NFL-playoffs')?.predictions as NFLBracket,
-          locked: true,
-        }
+        return this.user!.name === 'results'
+          ? {
+            tournament: this.competition.name,
+            predictions: this.users
+              ?.find((user) => user.name === 'results')
+              ?.predictions.find((prediction) => prediction.tournament === 'NFL-playoffs')
+              ?.predictions as NFLBracket,
+            locked: false,
+            tournamentPoints: 0,
+          }
+          : {
+            tournament: this.competition.name,
+            predictions: this.users
+              ?.find((user) => user.name === 'results')
+              ?.predictions.find((prediction) => prediction.tournament === 'NFL-playoffs')
+              ?.predictions as NFLBracket,
+            locked: true,
+            tournamentPoints: 0,
+          };
       } else {
-        return prediction
+        return prediction;
       }
-    })
-    console.log(newPredictions)
-    this.userService.updateUserPredictionsBackUp({
-      ...this.user!,
-      predictions: newPredictions
-    }).then(() => {
-      console.log('Updated successfully!');
-      this.userService.getUser(this.user!.name).then(user => this.user = user);
     });
+    console.log(newPredictions);
+    this.userService
+      .updateUserPredictionsBackUp({
+        ...this.user!,
+        predictions: newPredictions,
+      })
+      .then(() => {
+        console.log('Updated successfully!');
+        this.userService.getUser(this.user!.name).then((user) => (this.user = user));
+      });
   }
 
   unlockPredictionsForUser() {
-    const unlockedPredictions = this.user!.predictions.map(prediction => {
+    const unlockedPredictions = this.user!.predictions.map((prediction) => {
       if (prediction.tournament === this.competition.name) {
         return {
           tournament: this.competition.name,
           predictions: prediction.predictions,
           locked: false,
-        }
+        };
       } else {
         return prediction;
       }
-    })
-    this.userService.updateUserPredictions({
-      ...this.user!,
-      predictions: unlockedPredictions
-    }).then(() => {
-      console.log('Unlocked successfully!');
-      this.userService.getUser(this.user!.name).then(user => this.user = user);
-    })
+    });
+    this.userService
+      .updateUserPredictions({
+        ...this.user!,
+        predictions: unlockedPredictions,
+      })
+      .then(() => {
+        console.log('Unlocked successfully!');
+        this.userService.getUser(this.user!.name).then((user) => (this.user = user));
+      });
   }
 
   isTabLocked(player: User): boolean {
     if (player.name === this.user!.name) {
       return false;
     }
-    return !this.user?.predictions.find(prediction => prediction.tournament === 'NFL-playoffs')!.locked;
+    return !this.user?.predictions.find((prediction) => prediction.tournament === 'NFL-playoffs')!
+      .locked;
   }
 
   getResults(): NFLBracket {
-    return this.users?.find(user => user.name === 'results')?.predictions.find(prediction => prediction.tournament === 'NFL-playoffs')?.predictions as NFLBracket
+    return this.users
+      ?.find((user) => user.name === 'results')
+      ?.predictions.find((prediction) => prediction.tournament === 'NFL-playoffs')
+      ?.predictions as NFLBracket;
   }
 }
-
-
